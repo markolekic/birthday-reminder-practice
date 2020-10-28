53cd1acac81c2ca8fad4de4bb7f5237bcd4c97b9
Marko <markolekic985@gmail.com>
2020-10-28 09:45:56 +0100

    Updates

---
+++ precache-manifest.df3425e671dea801e054c97a81032827.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6533f97a73bb0874c7a11bb415b74f10",
+    "url": "/birthday-reminder-practice/index.html"
+  },
+  {
+    "revision": "9d43179b803f8a803f9d",
+    "url": "/birthday-reminder-practice/static/js/2.6ad8291f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/birthday-reminder-practice/static/js/2.6ad8291f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb4bd90f6ceaec6cc298",
+    "url": "/birthday-reminder-practice/static/js/main.427b822e.chunk.js"
+  },
+  {
+    "revision": "50355fbeee8601e0a811",
+    "url": "/birthday-reminder-practice/static/js/runtime-main.33720203.js"
+  }
+]);
